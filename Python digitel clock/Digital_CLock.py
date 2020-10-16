4aaa8244994cf57c75477164e0c1f7f25e1fe2c0
deepak110209 <72994035+deepak110209@users.noreply.github.com>
2020-10-17 01:34:29 +0530

    Update Digital_CLock.py
    Hi have updated in your digital clock colour and dimensions it's better now

--- Python digitel clock/Digital_CLock.py
+++ Python digitel clock/Digital_CLock.py
@@ -3,13 +3,13 @@ from tkinter import *
 
 root = Tk()
 root.title("Digital Clock")
-root.geometry("300x80") ## dimensions
+root.geometry("400x100") ## dimensions is best now 
 ## color 
-label = Label(root, font=("Black", 30, 'bold'), bg="Black", fg="yellow", bd =30)
+label = Label(root, font=("Black", 30, 'bold'), bg="Yellow", fg="Black", bd =30) ## now the clock color visible
 label.grid(row=0, column=1)
 ## function for clock time
 def clock():
-    text_input = time.strftime("%H:%M:%S")
+    text_input = time.strftime("%H:%M:%S") ## formet of Time in hours minutes and seconds
     label.config(text=text_input)
     label.after(200, clock)
     
