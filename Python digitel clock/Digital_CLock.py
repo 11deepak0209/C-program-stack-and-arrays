eea4fe8bc67839924dcbc8b018cd2ae88618eb3e
deepak110209 <72994035+deepak110209@users.noreply.github.com>
2020-10-17 01:11:46 +0530

    Update Digital_CLock.py
    Hi I have changed your dimension of clock and colour it's maybe better now and I am a beginner I have participated in hacktoberfest
    thanks
    Deepak

--- Python digitel clock/Digital_CLock.py
+++ Python digitel clock/Digital_CLock.py
@@ -3,9 +3,9 @@ from tkinter import *
 
 root = Tk()
 root.title("Digital Clock")
-root.geometry("300x80") ## dimensions
+root.geometry("400x100") ## dimensions should big 
 ## color 
-label = Label(root, font=("Black", 30, 'bold'), bg="Black", fg="yellow", bd =30)
+label = Label(root, font=("Black", 30, 'bold'), bg="Dark Blue", fg="yellow", bd =30) ## these color combination is good and visble
 label.grid(row=0, column=1)
 ## function for clock time
 def clock():
